53b70f1a231968958bc7ab426d5fd4404f736a3b
Lucia-Cai <cai.lucia04@gmail.com>
2024-11-24 02:12:04 -0500

    exxercises

--- CodeGym/frontend/src/components/ExerciseItem.js
+++ CodeGym/frontend/src/components/ExerciseItem.js
@@ -1,17 +1,25 @@
 import React from 'react'
+import {LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip} from "recharts"
 
-function ExerciseItem({name, rep, cur_weight, graph}) {
+function ExerciseItem({name, rep, cur_weight, data}) {
   return (
     <div className = "exerciseItem">
-        
-
-      <div>
+    <div>
         <h1>{name}</h1>
-        <p>{rep}</p>
-        <p>{cur_weight}</p>
-      </div>
+        <p>Reps: {rep}</p>
+        <p>Current Weight: {cur_weight}</p>
+    </div>
+    <div>
+        <LineChart width={300} height={300} data={data} margin={{top:5, right:20, bottom:5, left:0 }}>
+            <Line type="monotone" dataKey="value" stroke="#8884d8" />
+            <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+        </LineChart>
+    </div>
     </div>
   )
 }
 
-export default MenuItem
+export default ExerciseItem
