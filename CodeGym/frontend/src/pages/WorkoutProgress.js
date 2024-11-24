49697471687200554a770ee8b9a28b6fed428151
Marianne Romero <marianne.romero@mail.mcgill.ca>
2024-11-24 03:19:33 -0500

    graphs show up empty

--- CodeGym/frontend/src/pages/WorkoutProgress.js
+++ CodeGym/frontend/src/pages/WorkoutProgress.js
@@ -4,6 +4,54 @@ import '../styles/WorkoutProgress.css';
 import {useGetExercises} from "../axios.js";
 import ExerciseItem from '../components/ExerciseItem';
 
+
+const data = [
+  {
+    name: 'Page A',
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: 'Page B',
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'Page C',
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: 'Page D',
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: 'Page E',
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: 'Page F',
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: 'Page G',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
+
+
+
 function WorkoutProgress() {
   const navigate = useNavigate();
   const location = useLocation();
@@ -28,7 +76,7 @@ const { workout_name, workout_id } = location.state || {}; // Default to an empt
                 name = {exercise.name} 
                 rep = {exercise.reps} 
                 cur_weight = {exercise.weight}
-                data = {exercise.data}
+                data = {data}
                 />
             ))}
         </div>
